92763e278ad6d2c90425e052d26cdefbe3cc6d22
CathSigil <saidagil2808@gmail.com>
2026-05-01 18:17:01 +0700

    Add files via upload

---
+++ Modul 9/Bab 8/Matkomlan.java
@@ -0,0 +1,7 @@
+package Praktikum9_1;
+
+public class Matkomlan {
+    public double hitung(double t, double k, double uts, double uas) {
+    return (t * 0.25) + (k * 0.25) + (uts * 0.25) + (uas * 0.25);
+}
+}
---
+++ Modul 9/Bab 8/Probstat.java
@@ -0,0 +1,7 @@
+package Praktikum9_1;
+
+public class Probstat {
+    public double hitung(double t, double k, double uts, double uas) {
+    return (t * 0.1) + (k * 0.2) + (uts * 0.3) + (uas * 0.4);
+}
+}
