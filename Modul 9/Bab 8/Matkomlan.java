ad68b93fd8ec4c00b901f8d4a3e6b8a223d575dd
CathSigil <saidagil2808@gmail.com>
2026-05-01 18:14:26 +0700

    Delete Modul 9/Bab 8 directory

--- Modul 9/Bab 8/Matkomlan.java
+++
@@ -1,7 +0,0 @@
-package Praktikum9_1;
-
-public class Matkomlan {
-    public double hitung(double t, double k, double uts, double uas) {
-        return (t + k + uts + uas) / 4.0;
-    }
-}
--- Modul 9/Bab 8/Pemlan.java
+++
@@ -1,8 +0,0 @@
-package Praktikum9_1;
-
-    public class Pemlan {
-    public double hitung(double t, double k, double uts, double uas) {
-        return (t + k + uts + uas) / 4.0;
-    }
-    }
-
